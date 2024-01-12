04cd7ca74c33152013eb8340629d12acd636c68f
hasithaPerera2002 <hasithaperera2017@gmail.com>
2024-01-12 14:05:29 +0530

    configured express and mongo Db

--- index.js
+++ index.js
@@ -2,19 +2,17 @@ import express from "express";
 import mongoose from "mongoose";
 
 const app = express();
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-  mongoose
-    .connect("mongodb://localhost:27017/burger", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then(() => {
-      console.log("Connected to MongoDB");
-    })
-    .catch((err) => {
-      console.log("Connection failed", err);
+mongoose
+  .connect("mongodb://localhost:27017/burger", {})
+  .then(() => {
+    console.log("Connected to MongoDB");
+
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
     });
-});
+  })
+  .catch((err) => {
+    console.log("Connection to MongoDB failed", err);
+  });
 
 console.log("hii");
