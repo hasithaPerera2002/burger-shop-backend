444c1b92d2f0a9e83bed3e464e1c1bf8a8596ee2
hasithaPerera2002 <hasithaperera2017@gmail.com>
2024-02-01 22:28:11 +0530

    deployed

--- index.js
+++ index.js
@@ -34,7 +34,10 @@ mongoose
 
 app.use(express.json());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://ephemeral-lollipop-0ed82a.netlify.app"
+  );
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
