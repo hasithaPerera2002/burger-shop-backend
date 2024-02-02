1d80d04b4648777270e8019b09e33b89b9052be2
hasithaPerera2002 <hasithaperera2017@gmail.com>
2024-02-02 18:41:25 +0530

    config firebase to save images

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,27 +1,38 @@
 import multer from "multer";
 import CustomError from "../util/customErrorHandler.js";
+import {
+  getStorage,
+  ref,
+  getDownloadURL,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { initializeApp } from "firebase/app";
+import { config } from "../firebase/firebaseConfig.js";
+import { Express } from "express";
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads/");
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + "-" + file.originalname);
-  },
-});
+const router = Express.Router();
 
-const upload = multer({
-  storage: storage,
-  fileFilter: function (req, file, cb) {
-    if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
-      cb(null, true);
-    } else {
-      cb(new CustomError("Only .jpeg and .png format allowed!", 500), false);
+initializeApp(config.firebaseConfig);
+
+const fireStorage = getStorage();
+
+const storage = multer({ storage: multer.memoryStorage() });
+router.post("/", storage.single("image"), async (req, res, next) => {
+  const file = req.body.file || req.file;
+  if (!file) {
+    return next(new CustomError("Please upload a file", 400));
+  }
+  const imageRef = ref(fireStorage, "images/" + file.originalname + Date.now());
+  const metadata = {
+    contentType: file.mimetype,
+  };
+  await uploadBytesResumable(imageRef, file.buffer, metadata).then(
+    (snapshot) => {
+      getDownloadURL(snapshot.ref).then((downloadURL) => {
+        res.status(200).json({ downloadURL });
+      });
     }
-  },
-  limits: {
-    fileSize: 1024 * 1024 * 10,
-  },
+  );
 });
 
 export default upload;
