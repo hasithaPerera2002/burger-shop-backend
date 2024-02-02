e6d5d7dc013cc9e0c1c16ce8098204ac879ac13d
hasithaPerera2002 <hasithaperera2017@gmail.com>
2024-02-02 19:13:12 +0530

    bug fixed

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,40 +1,44 @@
-import multer from "multer";
-import CustomError from "../util/customErrorHandler.js";
 import {
   getStorage,
   ref,
-  getDownloadURL,
   uploadBytesResumable,
+  getDownloadURL,
 } from "firebase/storage";
 import { initializeApp } from "firebase/app";
 import firebaseConfig from "../firebase/firebaseConfig.js";
+import CustomError from "../util/customErrorHandler.js";
 
 initializeApp(firebaseConfig);
 
 const fireStorage = getStorage();
 
-const storage = multer({ storage: multer.memoryStorage() });
-const multerUpload = multer({ storage: storage });
-
 const upload = async (req, res, next) => {
-  const file = req.body.file || req.file;
-  if (!file) {
-    return next(new CustomError("Please upload a file", 400));
+  try {
+    const file = req.body.file || req.file;
+    if (!file) {
+      throw new CustomError("Please upload a file", 400);
+    }
+
+    const imageRef = ref(
+      fireStorage,
+      "images/" + file.originalname + Date.now()
+    );
+    const metadata = {
+      contentType: file.mimetype,
+    };
+
+    const snapshot = await uploadBytesResumable(
+      imageRef,
+      file.buffer,
+      metadata
+    );
+    const downloadURL = await getDownloadURL(snapshot.ref);
+
+    req.image = downloadURL;
+    next();
+  } catch (error) {
+    next(new CustomError("File upload failed: " + error.message, 500));
   }
-  const imageRef = ref(fireStorage, "images/" + file.originalname + Date.now());
-  const metadata = {
-    contentType: file.mimetype,
-  };
-  await uploadBytesResumable(imageRef, file.buffer, metadata)
-    .then((snapshot) => {
-      getDownloadURL(snapshot.ref).then((downloadURL) => {
-        req.image = downloadURL;
-        next();
-      });
-    })
-    .catch((err) => {
-      return next(new CustomError("File upload failed " + err.message, 500));
-    });
 };
 
-export default upload;
+export { upload };
